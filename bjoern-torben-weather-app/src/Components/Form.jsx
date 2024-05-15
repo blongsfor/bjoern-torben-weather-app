f28f8f4a6b29acb12150d582c69dcb3f6cf877e8
Björn <bjoern.n@mailbox.org>
2024-05-15 09:58:04 +0200

    add checkbox .checked event target remove checkbox.jsx file

--- bjoern-torben-weather-app/src/Components/Form.jsx
+++ bjoern-torben-weather-app/src/Components/Form.jsx
@@ -1,12 +1,12 @@
-import { Checkbox } from "./Checkbox";
-
 export default function Form({ onAddActivity }) {
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
+    const isChecked = event.target.elements["checkbox"].checked;
 
     console.log("submit: ", data);
+    console.log("is checked: ", isChecked);
     onAddActivity(data);
     event.target.reset();
     event.target.elements.name.focus();
@@ -18,7 +18,8 @@ export default function Form({ onAddActivity }) {
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input name="name"></input>
-        <Checkbox />
+        <label htmlFor="isForGoodWeather">Is For Good Weather</label>
+        <input type="checkbox" id="checkbox" />
         <button>Submit</button>
       </form>
     </>
