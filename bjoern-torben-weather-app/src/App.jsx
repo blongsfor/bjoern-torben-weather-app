475564a956f063f0c11cb818cfe68f1b7d700975
Björn <bjoern.n@mailbox.org>
2024-05-15 11:47:37 +0200

    only good weather items displayed

--- bjoern-torben-weather-app/src/App.jsx
+++ bjoern-torben-weather-app/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+/*import { useState } from "react";
 import "./App.css";
 import Form from "./Components/Form";
 import { List } from "./Components/List";
@@ -10,6 +10,11 @@ const InitialActivities = [
   { name: "Sisyphos", checked: true },
   { name: "drin bleiben", checked: false },
 ];
+
+const isGoodWeather = true;
+//const filteredActivities = InitialActivities.filter(
+//  (activity) => activity.isForGoodWeather === isGoodWeather
+//);
 
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
@@ -28,8 +33,62 @@ function App() {
           {activities.map((activity) => (
             <li key={activity.id}>
               <List
+                //  activities={filteredActivities}
+                //  isGoodWeather={isGoodWeather}
                 name={activity.name}
                 isForGoodWeather={activity.isChecked}
+                id={activity.id}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Form onAddActivity={handleAddActivity} />
+    </>
+  );
+}
+
+export default App; */
+
+import { useState } from "react";
+import "./App.css";
+import Form from "./Components/Form";
+import { List } from "./Components/List";
+import { uid } from "uid";
+import useLocalStorageState from "use-local-storage-state";
+
+const InitialActivities = [
+  { name: "Berghain", isForGoodWeather: true, checked: false },
+  { name: "Sisyphos", isForGoodWeather: true, checked: true },
+  { name: "drin bleiben", isForGoodWeather: false, checked: false },
+];
+
+const isGoodWeather = true;
+
+function App() {
+  const [activities, setActivities] = useLocalStorageState("activities", {
+    defaultValue: InitialActivities,
+  });
+
+  function handleAddActivity(newActivity) {
+    setActivities([...activities, { id: uid(), ...newActivity }]);
+  }
+
+  const filteredActivities = activities.filter(
+    (activity) => activity.isForGoodWeather === isGoodWeather
+  );
+
+  console.log("activities", activities);
+  return (
+    <>
+      <div>
+        <h1>Weather App</h1>
+        <ul>
+          {filteredActivities.map((activity) => (
+            <li key={activity.id}>
+              <List
+                name={activity.name}
+                isForGoodWeather={activity.isForGoodWeather}
                 id={activity.id}
               />
             </li>
--- bjoern-torben-weather-app/src/Components/Form.jsx
+++ bjoern-torben-weather-app/src/Components/Form.jsx
@@ -1,4 +1,4 @@
-export default function Form({ onAddActivity }) {
+/*export default function Form({ onAddActivity }) {
   function handleSubmit(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -20,6 +20,34 @@ export default function Form({ onAddActivity }) {
         <input name="name"></input>
         <label htmlFor="isForGoodWeather">Is For Good Weather</label>
         <input type="checkbox" id="checkbox" />
+        <button>Submit</button>
+      </form>
+    </>
+  );
+} */
+
+export default function Form({ onAddActivity }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    const isForGoodWeather = event.target.elements["isForGoodWeather"].checked;
+
+    console.log("submit: ", data);
+    console.log("isForGoodWeather: ", isForGoodWeather);
+    onAddActivity({ ...data, isForGoodWeather });
+    event.target.reset();
+    event.target.elements.name.focus();
+  }
+
+  return (
+    <>
+      <h1>Add new Activity</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input name="name" required></input>
+        <label htmlFor="isForGoodWeather">Is For Good Weather</label>
+        <input type="checkbox" name="isForGoodWeather" id="isForGoodWeather" />
         <button>Submit</button>
       </form>
     </>
