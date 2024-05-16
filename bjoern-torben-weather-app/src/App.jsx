3a97bf3cc6d53f9da01e2492cb7644311e942f86
Björn <bjoern.n@mailbox.org>
2024-05-16 09:44:00 +0200

    add location selection

--- bjoern-torben-weather-app/src/App.jsx
+++ bjoern-torben-weather-app/src/App.jsx
@@ -1,3 +1,4 @@
+//////////////////////////////////////IMPORTS//////////////////////////////////////
 import { useState } from "react";
 import "./App.css";
 import Form from "./Components/Form";
@@ -6,71 +7,118 @@ import { uid } from "uid";
 import useLocalStorageState from "use-local-storage-state";
 import { useEffect } from "react";
 import "./Components/Checkbox.css";
+import City from "./Components/City";
 
+//////////////////////////////////////INITIAL DATA BEFORE API//////////////////////////////////////
 const InitialActivities = [
-  { id: 1, name: "Berghain", isForGoodWeather: true },
-  { id: 2, name: "Sisyphos", isForGoodWeather: true },
-  { id: 3, name: "drin bleiben", isForGoodWeather: false },
+  {
+    id: 1,
+    name: "Berghain",
+    categoriesOptions: "social-cultural",
+    isForGoodWeather: true,
+  },
+  {
+    id: 2,
+    name: "Sisyphos",
+    categoriesOptions: "social-cultural",
+    isForGoodWeather: true,
+  },
+  {
+    id: 3,
+    name: "drin bleiben",
+    categoriesOptions: "leisure",
+    isForGoodWeather: false,
+  },
 ];
-
+//////////////////////////////////////APP COMPONENT//////////////////////////////////////
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", {
     defaultValue: InitialActivities,
   });
-
   const [isGoodWeather, setIsGoodWeather] = useState(true);
+  const [categorizedActivities, setCategorizedActivities] = useState([]);
   const [condition, setCondition] = useState("");
   const [temperature, setTemperature] = useState(0);
-  const [location, setLocation] = useState("");
-
-  // const isGoodWeather = true;
-
-  const filteredActivities = activities.filter(
-    (activity) => activity.isForGoodWeather === isGoodWeather
+  const [selectedCity, setSelectedCity] = useState("rainforest");
+  const filteredActivitiesByWeather = activities.filter(
+    (activity) =>
+      isGoodWeather ? activity.isForGoodWeather : !activity.isForGoodWeather // alternative: return activity.isForGoodWeather === isGoodWeather;
   );
-
+  // -------------------- this function adds new entries and categorizes them into the tabs -------------
   function handleAddActivity(newActivity) {
+    const { categoriesOptions } = newActivity;
     setActivities([...activities, { id: uid(), ...newActivity }]);
+    const updatedCategorizedActivities = categorizedActivities.map(
+      (category) => {
+        if (category.category === categoriesOptions) {
+          return {
+            category: categoriesOptions,
+            activities: [...category.activities, newActivity],
+          };
+        }
+        return category;
+      }
+    );
+    if (
+      !updatedCategorizedActivities.some(
+        (category) => category.category === categoriesOptions
+      )
+    ) {
+      setCategorizedActivities([
+        ...updatedCategorizedActivities,
+        {
+          category: categoriesOptions,
+          activities: [newActivity],
+        },
+      ]);
+    } else {
+      setCategorizedActivities(updatedCategorizedActivities);
+    }
   }
-
+  // -------------- this function deletes activities on button click ----------------------
   function handleDeleteActivity(idActivity) {
-    setActivities(activities.filter((activity) => activity.id != idActivity));
+    setActivities(activities.filter((activity) => activity.id !== idActivity));
   }
-
+  // ---------------- this function selects the location for the fetching of the weather ----------------------
+  function handleSelectCity(selectedCity) {
+    setSelectedCity(selectedCity);
+  }
+  // ------------------ fetch API DATA ---------------------
   useEffect(() => {
     async function getIsGoodWeather() {
       try {
         const response = await fetch(
-          "https://example-apis.vercel.app/api/weather"
+          `https://example-apis.vercel.app/api/weather/${selectedCity}`
         );
         let fetchedData = await response.json();
         console.log("weather", fetchedData);
         setIsGoodWeather(fetchedData.isGoodWeather);
         setCondition(fetchedData.condition);
         setTemperature(fetchedData.temperature);
-        setLocation(fetchedData.location);
       } catch (err) {
-        console.log(
-          "Failed to fetch weather data, defaulting weather to bad weather: ",
-          err
-        );
+        console.log("couldn't load data", err);
       }
     }
     getIsGoodWeather();
     const interval = setInterval(getIsGoodWeather, 5000);
     return () => clearInterval(interval);
-  }, []);
-
-  console.log("activities", activities);
+  }, [selectedCity]);
+  // ------------------ display components ---------------------
   return (
     <>
       <div>
+        <City onSelectCity={handleSelectCity} />
         <h1 className="heading">
           <span>{condition}</span>
           <span>{temperature} °C</span>
         </h1>
-        <div>
-          {filteredActivities.map((activity) => (
+        <p>
+          {isGoodWeather
+            ? "The weather is awesome! Go outside and:"
+            : "Bad weather outside! Here's what you can do now:"}
+        </p>
+        <ul>
+          {filteredActivitiesByWeather.map((activity) => (
             <li key={activity.id}>
               <List
                 name={activity.name}
@@ -80,11 +128,10 @@ function App() {
               />
             </li>
           ))}
-        </div>
+        </ul>
       </div>
       <Form onAddActivity={handleAddActivity} />
     </>
   );
 }
-
 export default App;
---
+++ bjoern-torben-weather-app/src/Components/City.jsx
@@ -0,0 +1,28 @@
+// ------------------ this component returns a dropdown menu  of locations ---------------------
+import { useState } from "react";
+export default function City({ onSelectCity }) {
+  const [selectCityValue, setSelectCityValue] = useState("rainforest");
+  function handleChangeCity(event) {
+    const selectedCity = event.target.value.toLowerCase();
+    setSelectCityValue(selectedCity);
+    onSelectCity(selectedCity);
+  }
+  return (
+    <>
+      <form name="city-form">
+        <label htmlFor="location-names">Choose a location:</label>
+        <select
+          name="location-names"
+          id="location-names"
+          value={selectCityValue}
+          onChange={handleChangeCity}
+        >
+          <option value="europe">Europe</option>
+          <option value="arctic">Arctic</option>
+          <option value="Sahara">Sahara</option>
+          <option value="rainforest">Rainforest</option>
+        </select>
+      </form>
+    </>
+  );
+}
